07df2e6e9b87ddce3575e29f307c74e5ea6f5ad8
Tonyk <tonykurosaki117@gmail.com>
2021-03-09 23:59:07 -0600

    Landing terminada

---
+++ src/routes/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MainPage from "../containers/MainPage";
+import Layout from "../components/Layout";
+import "../assets/styles/index.css";
+import NotFound from "../containers/NotFound";
+import Player from "../containers/Player";
+
+const App = () => {
+    return(
+         <BrowserRouter>
+            <Layout>
+                <Switch>
+                    <Route exact path="/" component={MainPage}/>
+                    <Route exact path="/player" component={Player}/>
+                    <Route component={NotFound}/>
+                </Switch>
+            </Layout>
+        </BrowserRouter>
+    );
+};
+    
+      
+export default App;
